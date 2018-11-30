22790631398464eac71ca6e3722cb594872b01d7
Richard Krasso <rkrasso@bellevue.edu>
2018-11-30 13:17:50 -0600

    starter-code

---
+++ week-2/starter-code/assignment-2.4.js
@@ -0,0 +1,15 @@
+/*
+  // Below is what I am expecting to see when I run your program - remove this before submitting your work
+  // 50% of you will not read this and lose 25% of the awardable points.
+
+  FirstName LastName
+  Assignment 2.4
+  Today's Date
+
+  Hello my name is <concatenated full name>!
+
+  Today's date is <formatted date> and the current temperature is <formatted number with 1 fixed decimal position>
+
+  I am <parsed int> years old and my savings account goal is <parsed float value> dollars.
+
+*/
---
+++ week-3/starter-code/exercise-3.3.js
@@ -0,0 +1,10 @@
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 3.3
+  Today's Date
+
+  // Expected output
+  The enter key was pressed
+*/
