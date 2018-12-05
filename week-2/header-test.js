730a27fb82a7dec015402dde97cf57ea21ecb78e
Richard Krasso <rkrasso@bellevue.edu>
2018-12-05 10:13:43 -0600

    updated date localization to en-US.

--- week-2/header-test.js
+++ week-2/header-test.js
@@ -7,6 +7,6 @@
 ;===========================================
 */
 
-var header = require('./header.js');
+const header = require('./header.js');
 
 console.log(header.display("Robert", "Frost", "Week 2"));
